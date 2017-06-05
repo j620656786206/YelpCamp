4722ab6ed741a1abd20c8fa00ab63df4b1e42512
CW Yu <j620656786206@gmail.com>
2017-06-05 23:09:46 +0000

    setup databaseurl

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ var commentsRoutes    = require("./routes/comments"),
     campgroundsRoutes = require("./routes/campgrounds"),
     indexRoutes        = require("./routes/index");
 
-//mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://cwyu:12345@ds163681.mlab.com:63681/cwyu_yelpcamp");
+mongoose.connect(process.env.DATABASEURL);
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
